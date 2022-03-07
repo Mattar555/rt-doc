e626aa9ec0522e725a865b8de056b5e9a8983250
Marwan Attar <marwan.attar@ibm.com>
2022-03-07 16:56:25 +1100

    Remove scripts

--- scripts/post-deploy.sh
+++
@@ -1,2 +0,0 @@
-oc get DeploymentConfig web -n rt-doc -o json | jq '.spec.template.spec.containers[0].resources.limits.memory = "14Mi" ' | oc apply -f -
-oc get DeploymentConfig dbos-console -n rt-doc -o json | jq '.spec.template.spec.containers[0].resources.limits.memory = "14Mi" ' | oc apply -f -
